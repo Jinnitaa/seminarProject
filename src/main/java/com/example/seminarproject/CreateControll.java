19c50f9944cd6773bfc4a37ac7769e4cf3876ad2
Jinnitaa <phalpanhanyta12d@gmail.com>
2023-03-16 22:52:16 +0700

    added4 commit

--- src/main/java/com/example/seminarproject/CreateControll.java
+++ src/main/java/com/example/seminarproject/CreateControll.java
@@ -3,10 +3,8 @@ package com.example.seminarproject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+
 
 public class CreateControll {
     //Button field
